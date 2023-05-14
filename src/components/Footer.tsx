126c36b0ff8db87bd6e2bca098fedf240859d1c5
mnq11 <mnq_77@yahoo.com>
2023-05-14 20:51:21 +0200

    make it better

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import '../style/Footer.css';
 
 const Footer: React.FC = () => {
     return (
         <footer className="footer">
             <div className="footer-container">
-                {/*<div className="footer-section">*/}
-                {/*    <h3>Quick Links</h3>*/}
-                {/*    <ul className="footer-links">*/}
-                {/*        <li><Link to="/">Home</Link></li>*/}
-                {/*        <li><Link to="/about">About</Link></li>*/}
-                {/*        <li><Link to="/product">Product</Link></li>*/}
-                {/*        <li><Link to="/customer">Customer Service</Link></li>*/}
-                {/*        <li><Link to="/contact">Contact Us</Link></li>*/}
-                {/*    </ul>*/}
-                {/*</div>*/}
+
                 <div className="footer-section">
                     <h3>Contact Info</h3>
                     <p>
